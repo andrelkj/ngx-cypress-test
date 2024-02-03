c2643472bba75a003352ee95ada2c22b289d7daf
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-02-03 16:25:38 -0300

    add elements handleing and DOM manipulation

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,35 +1,95 @@
 /// <reference types="cypress" />
 
 describe("First test suite", () => {
-
   it("first test", () => {
-
-    cy.visit('/')
-    cy.contains('Forms').click()
-    cy.contains('Form Layouts').click()
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
 
     //by Tag name
-    cy.get('input')
+    cy.get("input");
 
     //by ID
-    cy.get('#inputEmail1')
+    cy.get("#inputEmail1");
 
     //by Class value
-    cy.get('.input-full-width')
+    cy.get(".input-full-width");
 
     //by Attribute name
-    cy.get('[placeholder="Email"]')
+    cy.get('[placeholder="Email"]');
 
     //by entire Class value
-    cy.get('[class="input-full-width size-medium shape-rectangle"]')
+    cy.get('[class="input-full-width size-medium shape-rectangle"]');
 
     //by two atrributes
-    cy.get('[placeholder="Email"][fullwidth]')
+    cy.get('[placeholder="Email"][fullwidth]');
 
     //by tag, atrribute id and class
-    cy.get('input[placeholder="Email"]#inputEmail1.input-full-width')
+    cy.get('input[placeholder="Email"]#inputEmail1.input-full-width');
 
     //by cypress test ID
-    cy.get('[data-cy="imputEmail1"]')
+    cy.get('[data-cy="imputEmail1"]');
   });
-})
+
+  it("second test", () => {
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
+
+    //Theory
+    // get() - find elements on the page by locator globally
+    // find() - find child elements by locator
+    // contains() - find HTML text and by text and locator
+
+    cy.contains("Sign in");
+    cy.contains('[status="warning"]', "Sign in");
+
+    // find parement elements
+    cy.contains("nb-card", "Horizontal form").find("button");
+    cy.contains("nb-card", "Horizontal form").contains("Sign in");
+    cy.contains("nb-card", "Horizontal form").get("button"); // get always find all matching elements
+
+    // cypress chains and DOM
+    cy.get("#inputEmail3")
+      .parents("form")
+      .find("button")
+      .should("contain", "Sign in") // check for element text
+      .parents("form")
+      .find("nb-checkbox")
+      .click();
+  });
+
+  it.only("save subject of the command", () => {
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
+
+    cy.contains("nb-card", "Using the Grid")
+      .find('[for="inputEmail1"]')
+      .should("contain", "Email");
+    cy.contains("nb-card", "Using the Grid")
+      .find('[for="inputPassword2"]')
+      .should("contain", "Password");
+
+    // CANNOT DO THINGS LIKE THIS
+    // const usingTheGrid = cy.contains("nb-card", "Using the Grid");
+    // usingTheGrid.find('[for="inputEmail1"]').should("contain", "Email");
+    // usingTheGrid.find('[for="inputPassword2"]').should("contain", "Password");
+
+    // Alternatives to assigning variables in Cypress
+    // 1 - Cypress Alias - global variable that can be used whenever required once defined
+    cy.contains("nb-card", "Using the Grid").as("usingTheGrid");
+    cy.get("@usingTheGrid")
+      .find('[for="inputEmail1"]')
+      .should("contain", "Email");
+    cy.get("@usingTheGrid")
+      .find('[for="inputPassword2"]')
+      .should("contain", "Password");
+
+    // 2 - Cypres then() methods - method is only visible inside the function though
+    cy.contains("nb-card", "Using the Grid").then((usingTheGridForm) => {
+      cy.wrap(usingTheGridForm).find('[for="inputEmail1"]').should("contain", "Email");
+      cy.wrap(usingTheGridForm).find('[for="inputPassword2"]').should("contain", "Password");
+    });
+  });
+});
