88f6ed8547a6611500a369fa3900d941939968a6
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-02-03 17:41:20 -0300

    add radio button validation and hidden elements handling

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -97,7 +97,7 @@ describe("First test suite", () => {
     });
   });
 
-  it.only("extract text values", () => {
+  it("extract text values", () => {
     cy.visit("/");
     cy.contains("Forms").click();
     cy.contains("Form Layouts").click();
@@ -131,4 +131,28 @@ describe("First test suite", () => {
         expect(property).to.equal('test@test.com')
       })
   });
+
+  it('radio buttons', () => {
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
+
+    // redio button methods on cypress requires the element to have type radio to work
+    cy.contains("nb-card", "Using the Grid").find('[type="radio"]').then( radioButtons => {
+      cy.wrap(radioButtons).eq(0).check({force: true}).should('be.checked') //force: true skipping the element wait - it assumes the element will be there
+      cy.wrap(radioButtons).eq(1).check({force: true})
+      cy.wrap(radioButtons).eq(0).should('not.be.checked')
+      cy.wrap(radioButtons).eq(2).should('be.disabled')
+    })
+  })
+
+  it.only('checkboxes', () => {
+    cy.visit("/");
+    cy.contains("Modal & Overlays").click();
+    cy.contains("Toastr").click();
+
+    cy.get('[type="checkbox"]').uncheck({force: true})
+    cy.get('[type="checkbox"]').eq(0).click({force: true})
+    cy.get('[type="checkbox"]').eq(1).check({force: true})
+  })
 });
