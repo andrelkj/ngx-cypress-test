067f3ed3d9d95310112bb3c72d35ac7ef4aff6f9
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-02-13 02:45:42 -0300

    add e2e test with page objects

--- cypress/e2e/testWithPageObjects.js
+++ cypress/e2e/testWithPageObjects.js
@@ -1,16 +1,37 @@
-import { navigateTo } from "../support/page_objects/navigationPage"
+import { onDatePickerPage } from "../support/page_objects/datepickerPage";
+import { onFormLayoutsPage } from "../support/page_objects/formLayoutsPage";
+import { navigateTo } from "../support/page_objects/navigationPage";
+import { onSmartTablePage } from "../support/page_objects/smartTablePage";
 
-describe('Test With Page Objects', () => {
+describe("Test With Page Objects", () => {
+  beforeEach("open application", () => {
+    cy.visit("/");
+  });
 
-  beforeEach('open application', () => {
-    cy.visit('/')
-  })
+  it("verify navigation across the pages", () => {
+    navigateTo.formLayoutsPage();
+    navigateTo.datepickerPage();
+    navigateTo.toastrPage();
+    navigateTo.smartTablePage();
+    navigateTo.tooltipPage();
+  });
 
-  it('verify navigation across the pages', () => {
-    navigateTo.formLayoutsPage()
-    navigateTo.datepickerPage()
-    navigateTo.toastrPage()
+  it.only("should submit Inline and Basic form and select tomorrow date in the calendar", () => {
+    navigateTo.formLayoutsPage();
+    onFormLayoutsPage.submitInlineFormWithNameAndEmail(
+      "Artem",
+      "test@test.com"
+    );
+    onFormLayoutsPage.submitInlineFormWithNameAndPassword(
+      "test@test.com",
+      "password"
+    );
+    navigateTo.datepickerPage();
+    onDatePickerPage.selectCommonDatepickerDateFromToday(1);
+    onDatePickerPage.selectDatepickerWithRangeFromToday(7, 14);
     navigateTo.smartTablePage()
-    navigateTo.tooltipPage()
-  })
-})
+    onSmartTablePage.addNewRecordwithFirstAndLastName('Artem', 'Bondar')
+    onSmartTablePage.updateAgeByFirstName('Artem', '35')
+    onSmartTablePage.deleteRowByIndex(1)
+  });
+});
